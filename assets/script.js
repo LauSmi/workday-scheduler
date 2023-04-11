9d31243a3a9f906e341d7716bd21e98a361c4b4d
laurens <lauren.smi001@gmail.com>
2023-04-11 15:50:06 -0400

    refined event listener and time blocks

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   console.log('Get it done!');
 
   //Code that displays the current date in the header of the page.
-  let nowDate = dayjs().format('dddd MMMM DD, YYYY, hh:MMA');
+  let nowDate = dayjs().format('dddd MMMM DD, YYYY, h:mm A');
   let displayDate = document.getElementById('currentDay');
   displayDate.innerHTML = nowDate;
   let currentHour = dayjs().format('HH');
@@ -24,25 +24,37 @@ $(document).ready(function () {
     }
   });
 
-  //Listener for click events on the save button.
-  $('.saveBtn').on('click', function (event) {
-    console.log(event.target);
-    var parentOfThingClickedOn = $(event.target).parent();
-    console.log(parentOfThingClickedOn);
-    var textSibling = parentOfThingClickedOn.children('textarea').val();
-    console.log(textSibling);
-    localStorage.setItem(parentOfThingClickedOn.attr('id'), textSibling);
+  // Change time block color based on current time
+  function updateTimeBlockColor() {
+    var currentHour = moment().hours();
+    $('.time-div').each(function () {
+      var timeBlockHour = parseInt($(this).attr('id').split('-')[1]);
+      if (timeBlockHour < currentHour) {
+        $(this).addClass('past');
+      } else if (timeBlockHour === currentHour) {
+        $(this).removeClass('past');
+        $(this).addClass('present');
+      } else {
+        $(this).removeClass('past');
+        $(this).removeClass('present');
+        $(this).addClass('future');
+      }
+    });
+  }
+
+  // Listener for click events on the save button.
+  $('.saveBtn').on('click', function () {
+    var value = $(this).siblings('.description').val();
+    var key = $(this).parent().attr('id');
+    localStorage.setItem(key, value);
   });
 
-  //Code to get any user input that was saved in localStorage.
-  $('.time-div').each(function () {
-    let id = $(this).attr('id');
-    $(`#${id} .description`).val(localStorage.getItem(id));
+  // Get stored data from localStorage
+  $('.description').each(function () {
+    var id = $(this).parent().attr('id');
+    $(this).val(localStorage.getItem(id));
   });
+
+  // Initialize time block color
+  updateTimeBlockColor();
 });
-
-// TEST-Set a value in local storage
-localStorage.setItem('test', 'hello world');
-
-// Retrieve the value from local storage and log it to the console
-console.log(localStorage.getItem('test')); // should output "hello world"
